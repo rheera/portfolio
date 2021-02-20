9e8feee5286cb9f24e636057a7c20517c2a0b8c2
rheera <rheera2@uwo.ca>
2021-02-19 21:15:51 -0500

    CSS
    - made it so hovering over tiles gives a description of the site

--- src/tile.js
+++ src/tile.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Tile = ({ id, name, url, pic, desc }) => {
-  const [isHovered, setHovered] = useState(false);
-
   return (
     <div className="column tile-block is-3-widescreen is-5-desktop is-5-tablet box mx-2">
-      <a
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        href={url}
-      >
-        <div>
+      <a href={url}>
+        <div className={"tile-container"}>
           <div className={"unhovered-tile"}>
             <h2 className="title">{name}</h2>
             <img className={"website-img"} src={pic} alt="website preview" />
           </div>
-          {isHovered && (
-            <div className="hovered-tile">
-              <p>{desc}</p>
-            </div>
-          )}
+          <div className="hovered-tile content is-small">
+            <p className={"tile-desc"}>{desc}</p>
+          </div>
         </div>
       </a>
     </div>
